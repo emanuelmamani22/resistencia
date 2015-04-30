4250bd2acd1b6fbf58a8f657bd199a2009e6dbbb
Emmanuel Mamani <emanuel_media15@hotmail.com>
2015-04-29 21:33:55 -0300

    Agregue la clase Resistencia

---
+++ resistencia/resistencias.py
@@ -0,0 +1,21 @@
+class Resistencias :
+    def __init__ ( self ):
+        self.color1=None
+        self.color2=None
+        self.color3=None
+        
+    def setcolor1 ( self,a ):
+        self.color1=a
+    def setcolor2 ( self,a ):
+        self.color2=a
+    def setcolor3 ( self,a ):
+        self.color3=a
+    def getcolor1 ( self ):
+        a=self.color1
+        return a
+    def getcolor1 ( self ):
+        a=self.color2
+        return a
+    def getcolor1 ( self ):
+        a=self.color3
+        return a
