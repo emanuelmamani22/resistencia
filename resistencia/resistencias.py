05f0f471f888f504f23010720e5661031389d3b1
Emmanuel Mamani <emanuel_media15@hotmail.com>
2015-05-02 18:47:19 -0300

    Agregue la funcion valor cero y funcion tolerancia

--- resistencia/resistencias.py
+++ resistencia/resistencias.py
@@ -3,6 +3,7 @@ class Resistencias : #Comienza la clase
         self.color1=None
         self.color2=None
         self.color3=None
+        self.color4=None
         
     def setcolor1 ( self,a ):
         self.color1=a
@@ -10,6 +11,8 @@ class Resistencias : #Comienza la clase
         self.color2=a
     def setcolor3 ( self,a ):
         self.color3=a
+    def setcolor4 ( self,a ):
+        self.color4=a
     
     def getcolor1 ( self ):
         a=self.color1
@@ -19,6 +22,9 @@ class Resistencias : #Comienza la clase
         return a
     def getcolor3 ( self ):
         a=self.color3
+        return a
+    def getcolor4 ( self ):
+        a=self.color4
         return a
 #Termina la clase
 
@@ -52,12 +58,50 @@ def colores (e): #Funcion colores con los colores de las resistencias comerciale
         print 'Blanco-Marron',#
     else  :
         print 'El numero ingresado no es parte del los valores comerciales de las resistencia'
-        menu()
+        menu() #fin de la funcion colores
         
+        
+def valorcero (a): #funcion valorcero son la cantidad de cero que va despues de los dos primeros numeros
+    cont=0
+    for i in a :
+        if i == '0' :
+            cont=cont+1 
+        
+    if cont == 0:
+        print '-Negro'
+    elif cont == 1 :
+        print '-Marron'
+    elif cont == 2 :
+        print '-Rojo'
+    elif cont == 3 :
+        print '-Naranja'
+    elif cont == 4 :
+        print '-Amarillo'
+    elif cont == 5 :
+        print '-Verde'
+    elif cont == 6 :
+        print '-Azul'
+    elif cont == 7 :
+        print '-Violeta'
+    elif cont == 8 :
+        print '-Gris'
+    elif cont == 9 :
+        print '-Blanco' #fin de la funcion valorcero
+        
+        
+def tolerancia (t): #funcion tolerancia
+    if t == '5%' :
+        print '-Dorado'
+    elif t == '10%' :
+        print '-Plata'
+    else  :
+        print "El valor ingresado no corresponde a ningun valor existente de tolerancia" 
+        menu() #fin da la funcion tolerancia
 
-def iteracionr (a): #Funcion itaracion
+def iteracionr (a,y): #Funcion itaracion
     R=Resistencias()
     a=a
+    y=y
     cont=0
     b=[]
     for i in a :
@@ -69,6 +113,7 @@ def iteracionr (a): #Funcion itaracion
             b.append(i)
         cont=cont+1
     R.setcolor3(b)
+    R.setcolor4(y)
     
     cont2=0
     r=[]
@@ -81,17 +126,16 @@ def iteracionr (a): #Funcion itaracion
             r.append(e)
         cont2=cont2+1
     
+    t=R.getcolor4()
+    
     colores(r)
-   
-def menu ( ):
+    valorcero(b)
+    tolerancia(t)
+    
+def menu ( ): #menu principal
     a=raw_input("Ingrese el valor de la recistencia: ")
-    iteracionr(a)
+    y=raw_input("Ingrese el valor de la tolerancia: ")
+    iteracionr(a,y)
 
 
 menu()
-
-
-
-
-
-
